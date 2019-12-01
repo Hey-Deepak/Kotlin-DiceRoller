bbc780f861e945015dd24b2c6272a064c60508d2
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-02 04:46:05 +0530

    Task 2 completed.

--- app/src/main/java/com/dc/diceroller/MainActivity.kt
+++ app/src/main/java/com/dc/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.dc.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rollButton : Button = findViewById(R.id.roll_button);
+        rollButton.text = "let's roll"
     }
 }
