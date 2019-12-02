6af71af4ef9e8beedc428b4165b5a633a789a613
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-02 06:05:10 +0530

    Now you can play Dice Roller :-)

--- app/src/main/java/com/dc/diceroller/MainActivity.kt
+++ app/src/main/java/com/dc/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ package com.dc.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rollButton: Button = findViewById(R.id.roll_button);
+        val rollButton: Button = findViewById(R.id.roll_button);
         rollButton.setOnClickListener() {
             rollDice();
 
@@ -21,9 +22,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        var randomInt = Random.nextInt(6) + 1;
-        resultText.text = randomInt.toString();
+
+        val randomInt = Random.nextInt(6) + 1;
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1;
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3;
+            4 -> R.drawable.dice_4;
+            5 -> R.drawable.dice_5;
+            else -> R.drawable.dice_6;
+        }
+        val diceImage : ImageView = findViewById(R.id.dice_image);
+        diceImage.setImageResource(drawableResource);
     }
 
 }
